e338b799b5d40e317eac4b265b5a884b865b318c
IsaLaLa <thessan.l.93@gmail.com>
2020-03-18 13:50:54 +0100

    ComplexBatch: fortsatt testning.

--- src/batches/ComplexBatch.java
+++ src/batches/ComplexBatch.java
@@ -158,12 +158,12 @@ public class ComplexBatch {
 		ps[17].write(zeros);
 		//executeCheckPoint(true, 17);
 
-		//----------- CORRECT SO FAR ---------------
-
 		ps[18] = m.alloc(40);
 		//ps[18].write(range(1, 40));
 		ps[18].write(zeros);
-		executeCheckPoint(true, 18);
+		//executeCheckPoint(true, 18);
+
+		//----------- CORRECT SO FAR ---------------
 
 		ps[19] = m.alloc(5);
 		//ps[19].write(range(11, 15));
