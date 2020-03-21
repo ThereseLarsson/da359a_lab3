30a4e9c70ec7a3ecb938ba1abd478f1809ad3bbf
IsaLaLa <thessan.l.93@gmail.com>
2020-03-21 19:57:35 +0100

    ComplexBatch, BestFit: fortsatt testning.

--- src/batches/ComplexBatch.java
+++ src/batches/ComplexBatch.java
@@ -107,15 +107,15 @@ public class ComplexBatch {
 		ps[9].write(zeros);
 		//executeCheckPoint(true, 9);
 
-        //----------- BestFit: CORRECT SO FAR ---------------
-
 		m.release(ps[6]);
-		executeCheckPoint(false, 6);
+		//executeCheckPoint(false, 6);
+
+        //----------- BestFit: CORRECT SO FAR ---------------
 
 		ps[10] = m.alloc(40);
 		//ps[10].write(range(301, 340));
 		ps[10].write(zeros);
-		//executeCheckPoint(true, 10);
+		executeCheckPoint(true, 10);
 
 		//-----------------
 
